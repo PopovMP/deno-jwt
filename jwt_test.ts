f34e16809812146efda1cc9474bc68139cf10a06
Miroslav Popov <info@forexsb.com>
2024-12-20 17:14:23 +0200

    Use Node test.

--- jwt_test.ts
+++ jwt_test.ts
@@ -1,18 +1,9 @@
+import { test } from "node:test";
+import { ok, strictEqual } from "node:assert";
+
 import { createJwt, getJwtPayload, isJwtExpired, isJwtValid } from "./jwt.ts";
 
-function assertEquals(actual: string, expected: string, msg?: string): void {
-  if (actual !== expected) {
-    throw new Error(msg || `Expected "${expected}", got "${actual}"`);
-  }
-}
-
-function isOk(value: boolean, msg?: string): void {
-  if (!value) {
-    throw new Error(msg || `Expected true, got ${value}`);
-  }
-}
-
-Deno.test("createJwt()", async () => {
+test("createJwt()", async () => {
   const payload = {
     "sub": "1234567890",
     "name": "John Doe",
@@ -23,10 +14,10 @@ Deno.test("createJwt()", async () => {
   const expected = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9." +
     "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ." +
     "qm9F4njElMyEvCFcXqH5MwGowpoDjRt91mIWyOUr-7s";
-  assertEquals(actual, expected);
+  strictEqual(actual, expected);
 });
 
-Deno.test("createJwt() 2", async () => {
+test("createJwt() 2", async () => {
   const payload = {
     "iss": "Online JWT Builder",
     "iat": 1725540872,
@@ -51,10 +42,10 @@ Deno.test("createJwt() 2", async () => {
     "b2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9" +
     "qZWN0IEFkbWluaXN0cmF0b3IiXX0." +
     "E9fgo0_bRYyz-yb6m5QWTtY81Lt4KcPOZQJlayWWuhE";
-  assertEquals(actual, expected);
+  strictEqual(actual, expected);
 });
 
-Deno.test("isJwtValid()", async () => {
+test("isJwtValid()", async () => {
   const jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9." +
     "eyJpc3MiOiJmb3JleHNiLmNvbSIsImlhdCI6MTcyNTU0MDg3MiwiZXh" +
     "wIjoxNzU3MDc2ODcyLCJhdWQiOiJmb3JleHNiLmNvbS9lYS1zdHVkaW" +
@@ -62,10 +53,10 @@ Deno.test("isJwtValid()", async () => {
     "I0PqXCFyjlrFLgzTg_H2aPEbsOfPGNJJPwnLfG2KCe4";
   const key = "supersecret";
   const actual = await isJwtValid(jwt, key);
-  isOk(actual);
+  ok(actual);
 });
 
-Deno.test("getJwtPayload()", () => {
+test("getJwtPayload()", () => {
   const jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9." +
     "eyJpc3MiOiJmb3JleHNiLmNvbSIsImlhdCI6MTcyNTU0MDg3MiwiZXh" +
     "wIjoxNzU3MDc2ODcyLCJhdWQiOiJmb3JleHNiLmNvbS9lYS1zdHVkaW" +
@@ -79,10 +70,10 @@ Deno.test("getJwtPayload()", () => {
     "aud": "forexsb.com/ea-studio",
     "sub": "info@forexsb.com",
   };
-  assertEquals(JSON.stringify(actual), JSON.stringify(expected));
+  strictEqual(JSON.stringify(actual), JSON.stringify(expected));
 });
 
-Deno.test("isJwtExpired() given not-expired", async () => {
+test("isJwtExpired() given not-expired", async () => {
   const nowSec = Math.floor(Date.now() / 1000);
   const payload = {
     "sub": "1234567890",
@@ -92,10 +83,10 @@ Deno.test("isJwtExpired() given not-expired", async () => {
   };
   const key = "qwertyuiopasdfghjklzxcvbnm123456";
   const jwt = await createJwt(payload, key);
-  isOk(!isJwtExpired(jwt));
+  ok(!isJwtExpired(jwt));
 });
 
-Deno.test("isJwtExpired() given expired", async () => {
+test("isJwtExpired() given expired", async () => {
   const nowSec = Math.floor(Date.now() / 1000);
   const payload = {
     "sub": "1234567890",
@@ -105,5 +96,5 @@ Deno.test("isJwtExpired() given expired", async () => {
   };
   const key = "qwertyuiopasdfghjklzxcvbnm123456";
   const jwt = await createJwt(payload, key);
-  isOk(isJwtExpired(jwt));
+  ok(isJwtExpired(jwt));
 });
